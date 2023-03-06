588734b7e9850bb07acc4cd696661e4e12d33e9b
darren.lee <darren.lee@storeberry.ai>
2023-03-05 19:44:12 -0500

    fix:sfssldf

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -4,7 +4,7 @@ import InputWrapper from './InputWrapper';
 export default function Header(props) {
     return (
         <header>
-            <h1>My first Git hub task----to do</h1>
+            <h1>sfjslfjlsjflsa</h1>
             <InputWrapper {...props}/>
         </header>
     );
