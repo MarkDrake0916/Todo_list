950180cff76fe83b097b9c240c239ad78e715207
darren.lee <darren.lee@storeberry.ai>
2023-03-07 18:50:37 -0500

    fix:mark-victory

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -4,7 +4,7 @@ import InputWrapper from './InputWrapper';
 export default function Header(props) {
     return (
         <header>
-            <h1>Hello Markdrkae1</h1>
+            <h1>Hello Markdrake</h1>
             <InputWrapper {...props}/>
         </header>
     );
